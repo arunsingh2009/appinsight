c395fa83007bf1a4dbdecf7a8d144aa0276ce41f
Arun Singh <arun_si@HCL.COM>
2020-01-28 15:11:16 +0530

    app insight

---
+++ src/main/java/com/appinsight/appinsight/AppinsightApplication.java
@@ -0,0 +1,13 @@
+package com.appinsight.appinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppinsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppinsightApplication.class, args);
+	}
+
+}
